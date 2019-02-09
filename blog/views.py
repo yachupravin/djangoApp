9b329c1301e0d39d9fb025f89653952bf643b023
yachupravin <yachu.pravin@gmail.com>
2019-02-09 21:30:42 +0530

    updates3

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404,redirect
 from .models import Post
+from .form import PostForm
+from django.utils import timezone
 
 # Create your views here.
 def post_list(request):
@@ -9,3 +11,16 @@ def post_list(request):
 def post_detail(request, pk):
     posts = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': posts})
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+        return render(request, 'blog/post_new.html', {'form': form})
