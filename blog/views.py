4764f46e6d887d710d38b819f7f6770c306949e6
yachupravin <yachu.pravin@gmail.com>
2019-02-09 20:49:05 +0530

    updates1

--- blog/views.py
+++ blog/views.py
@@ -5,3 +5,7 @@ from .models import Post
 def post_list(request):
     postsObject=Post.objects.all()
     return render(request, 'blog/post_list.html', {'posts':postsObject})
+
+def post_detail(request, pk):
+    posts = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': posts})
